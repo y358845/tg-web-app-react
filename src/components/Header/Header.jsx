90bb474953718b2638b05757d73f6e0d40af8fdd
irlandets1980 <irlandets1980@gmail.com>
2023-04-26 21:55:52 +0300

    second2

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Button from "../Button/Button";
+import {useTelegram} from "../../hooks/useTelegram";
+import './Header.css';
+
+const Header = () => {
+    const tg = window.Telegram.WebApp;
+    const onClose = () => {
+        tg.close()
+      }
+
+    return (
+        <div className={'header'}>
+            <Button onClick={onClose}>ЗакрытьСука!</Button>
+            <span className={'username'}>
+               {tg.initDataUnsafe?user?.username}
+            </span>
+        </div>
+    );
+};
+
+export default Header;

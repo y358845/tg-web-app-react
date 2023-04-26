0fe45985679400612c914fa78fba9bec82311841
irlandets1980 <irlandets1980@gmail.com>
2023-04-26 21:29:02 +0300

    second

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+const tg = window.Telegram.WebApp;
+
+function App() {
+  // useEffect(()=>{
+  //   tg.ready()
+  // },[])
+const onClose = () => {
+  tg.close()
+}
+  return (
+    <div className="App">
+   work
+   <button onClick={onClose}> Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
